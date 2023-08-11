a6bcb2d80a07a431f50a8406d24f00d011362629
Cristian Enguídanos Nebot <cristian.enguidanos.nebot@gmail.com>
2023-08-11 23:33:31 +0200

    refactor: naming

--- src/route.rs
+++ src/route.rs
@@ -13,7 +13,7 @@ use crate::{data, AppState};
 pub async fn stream(State(state): State<AppState>) -> impl IntoResponse {
     let (sender, stream) = hyper::body::Body::channel();
 
-    let sender = Arc::new(Mutex::new(sender));
+    let response_sender = Arc::new(Mutex::new(sender));
 
     tokio::spawn(async move {
         let documents = data::get_documents(&state.conn).await;
@@ -21,14 +21,17 @@ pub async fn stream(State(state): State<AppState>) -> impl IntoResponse {
         let mut tasks = tokio::task::JoinSet::new();
 
         for document in documents.into_iter() {
-            let sender = Arc::clone(&sender);
+            let response_sender = Arc::clone(&response_sender);
 
             tasks.spawn(async move {
-                let parsed = format!("Invoice {}", document.id);
+                let content = format!("Invoice {}", document.id);
 
-                let mut sender = sender.lock().await;
+                let mut response_sender_locked = response_sender.lock().await;
 
-                if let Err(err) = sender.send_data(bytes::Bytes::from(parsed)).await {
+                if let Err(err) = response_sender_locked
+                    .send_data(bytes::Bytes::from(content))
+                    .await
+                {
                     println!("{err}");
                 }
 
@@ -54,21 +57,24 @@ pub async fn stream_zip(State(state): State<AppState>) -> impl IntoResponse {
 
         let mut tasks = tokio::task::JoinSet::new();
 
-        let archive = Arc::new(Mutex::new(zipit::Archive::new(wx)));
+        let file_archive = Arc::new(Mutex::new(zipit::Archive::new(wx)));
 
         for document in documents.into_iter() {
-            let archive = Arc::clone(&archive);
+            let file_archive = Arc::clone(&file_archive);
 
             tasks.spawn(async move {
-                let filename = format!("{}.txt", document.id);
-                let content = format!("Invoice {}", document.id);
+                let file_name = format!("{}.txt", document.id);
+                let file_content = format!("Invoice {}", document.id);
 
-                let mut content_cursor = std::io::Cursor::new(bytes::Bytes::from(content));
+                let mut file_content_cursor = std::io::Cursor::new(file_content);
+                let mut file_archive = file_archive.lock().await;
 
-                let mut archive = archive.lock().await;
-
-                if let Err(error) = archive
-                    .append(filename, zipit::FileDateTime::now(), &mut content_cursor)
+                if let Err(error) = file_archive
+                    .append(
+                        file_name,
+                        zipit::FileDateTime::now(),
+                        &mut file_content_cursor,
+                    )
                     .await
                 {
                     println!("{error}");
@@ -82,10 +88,10 @@ pub async fn stream_zip(State(state): State<AppState>) -> impl IntoResponse {
             println!("Processed {id:?}");
         }
 
-        if let Ok(inner) = Arc::try_unwrap(archive) {
-            let archive = inner.into_inner();
+        if let Ok(inner) = Arc::try_unwrap(file_archive) {
+            let file_archive = inner.into_inner();
 
-            if let Err(error) = archive.finalize().await {
+            if let Err(error) = file_archive.finalize().await {
                 println!("{error}");
             }
         }
@@ -114,27 +120,30 @@ pub async fn stream_zip_and_gzip(State(state): State<AppState>) -> impl IntoResp
 
         let mut tasks = tokio::task::JoinSet::new();
 
-        let archive = Arc::new(Mutex::new(zipit::Archive::new(wx)));
+        let file_archive = Arc::new(Mutex::new(zipit::Archive::new(wx)));
 
         for document in documents.into_iter() {
-            let archive = Arc::clone(&archive);
+            let file_archive = Arc::clone(&file_archive);
 
             tasks.spawn(async move {
-                let filename = format!("{}.zip", document.id);
-                let content = format!("Invoice {}", document.id);
+                let file_name = format!("{}.zip", document.id);
+                let file_content = format!("Invoice {}", document.id);
 
                 let mut compressor =
                     flate2::write::GzEncoder::new(Vec::new(), flate2::Compression::best());
 
-                if compressor.write_all(content.as_bytes()).is_ok() {
-                    let compressed_content = compressor.finish().unwrap_or_default();
+                if compressor.write_all(file_content.as_bytes()).is_ok() {
+                    let file_content_compressed = compressor.finish().unwrap_or_default();
 
-                    let mut content_cursor = std::io::Cursor::new(compressed_content);
+                    let mut file_content_cursor = std::io::Cursor::new(file_content_compressed);
+                    let mut file_archive_locked = file_archive.lock().await;
 
-                    let mut archive = archive.lock().await;
-
-                    if let Err(error) = archive
-                        .append(filename, zipit::FileDateTime::now(), &mut content_cursor)
+                    if let Err(error) = file_archive_locked
+                        .append(
+                            file_name,
+                            zipit::FileDateTime::now(),
+                            &mut file_content_cursor,
+                        )
                         .await
                     {
                         println!("{error}");
@@ -149,10 +158,10 @@ pub async fn stream_zip_and_gzip(State(state): State<AppState>) -> impl IntoResp
             println!("Processed {id:?}");
         }
 
-        if let Ok(inner) = Arc::try_unwrap(archive) {
-            let archive = inner.into_inner();
+        if let Ok(inner) = Arc::try_unwrap(file_archive) {
+            let file_archive = inner.into_inner();
 
-            if let Err(error) = archive.finalize().await {
+            if let Err(error) = file_archive.finalize().await {
                 println!("{error}");
             }
         }
